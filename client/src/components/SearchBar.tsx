3857988c3f870481fa9551270479ec2084f464e9
l-stend <stendardo.lu@gmail.com>
2022-08-18 15:02:41 +0200

    feat: input field setup

--- client/src/components/SearchBar.tsx
+++ client/src/components/SearchBar.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
+import { useAppSelector } from '../utils/hooks';
 import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
 import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
 
 const SearchBar = () => {
+  const allItemsArr = useAppSelector((state) => state.allItems.allItemsArr);
+
   return (
     <AppBar position='sticky'>
-      {/* <Autocomplete></Autocomplete> */}
-      <h1>SearchBar</h1>
+      <Autocomplete
+        id='navbar-search-bar'
+        sx={{ width: '40vw' }}
+        renderInput={(params) => (
+          <TextField {...params} label='Search for ASIN' />
+        )}
+        options={allItemsArr}
+        getOptionLabel={(item) => item.asin}
+        renderOption={(props, item) => (
+          <Box component='li' {...props} key={item.asin}>
+            {item.asin}
+          </Box>
+        )}
+        // onInputChange={(e) => dispatch(filterList(e.target.value))}
+      />
     </AppBar>
   );
 };
