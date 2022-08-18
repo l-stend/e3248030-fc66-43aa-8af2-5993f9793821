082063727773351ca38e050cdbd3f2df856966e9
l-stend <stendardo.lu@gmail.com>
2022-08-18 10:31:23 +0200

    feat: provider setted and test api call

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,32 @@
 import React from 'react';
 import './App.css';
+import { useAppDispatch } from './utils/hooks';
+import { getItemsArr } from './features/allItemsSlice';
+// import { ProductView } from './components';
 
 function App() {
-  return <div className='App'></div>;
+  const dispatch = useAppDispatch();
+
+  // async function getData() {
+  //   let res = await fetch('https://pastebin.com/raw/eNPBe2Pk', {
+  //     method: 'GET',
+  //     mode: 'no-cors',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //   });
+  //   let data = await res.json();
+  //   return data;
+  // }
+
+  // getData().then((res) => console.log(res));
+
+  return (
+    <div className='App'>
+      <button onClick={() => dispatch(getItemsArr())}>api call</button>
+      {/* <ProductView /> */}
+    </div>
+  );
 }
 
 export default App;
